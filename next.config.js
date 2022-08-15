031ef64c195aa3a9cd9eff782336a27d1933ee10
Cam <github@lekienhoanh.dev>
2022-08-15 14:01:19 +0700

    update nextconfnig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: ["assets.example.com", "placeimg.com"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
