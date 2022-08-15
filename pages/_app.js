62102d0f15ca24190a95493ff3178d79b1fcee35
Restyled.io <commits@restyled.io>
2022-08-15 04:29:44 +0000

    Restyled by clang-format

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import "../styles/globals.css";
 
 // eslint-disable-next-line react-pug/prop-types
-function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+function MyApp({Component, pageProps}) {
+  return < Component { ...pageProps }
+  />;
 }
 
 export default MyApp;
