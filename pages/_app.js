b6aecdef438e40874926541aaeba98967f8ca324
Cam <github@lekienhoanh.dev>
2022-08-15 11:29:30 +0700

    fix eslint error

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
+// eslint-disable-next-line react-pug/prop-types
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
