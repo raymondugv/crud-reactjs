7af8fa57c9baa44fb2017deb52739326b292dbaf
Cam <github@lekienhoanh.dev>
2022-08-15 14:02:06 +0700

    added document

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Header from "../components/header";
+import Footer from "../components/footer";
+
+const Document = () => {
+	return (
+		<Html data-theme="lofi">
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="true"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+};
+
+export default Document;
