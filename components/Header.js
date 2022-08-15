a1f086a0ffd5716571dd338c3f9e4fff96b589db
Cam <github@lekienhoanh.dev>
2022-08-15 14:09:26 +0700

    update header

--- components/Header.js
+++ components/Header.js
@@ -4,47 +4,39 @@ const Header = () => {
 	return (
 		<div className="navbar bg-base-100">
 			<div className="flex-1">
-				<a className="btn btn-ghost normal-case text-xl">
-					CRUD Project
-				</a>
+				<a className="btn btn-ghost normal-case text-xl">daisyUI</a>
 			</div>
-			<div className="flex-none gap-2">
-				<div className="form-control">
-					<input
-						type="text"
-						placeholder="Search"
-						className="input input-bordered"
-					/>
-				</div>
-				<div className="dropdown dropdown-end">
-					<label
-						tabIndex="0"
-						className="btn btn-ghost btn-circle avatar"
-					>
-						<div className="w-10 rounded-full">
-							<Image
-								alt="image"
-								src="https://placeimg.com/80/80/people"
-								width={500}
-								height={500}
-							/>
-						</div>
-					</label>
-					<ul
-						tabIndex="0"
-						className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-					>
-						<li>
-							<a>Profile</a>
-						</li>
-						<li>
-							<a>Settings</a>
-						</li>
-						<li>
-							<a>Logout</a>
-						</li>
-					</ul>
-				</div>
+			<div className="flex-none">
+				<ul className="menu menu-horizontal p-0">
+					<li>
+						<a>Item 1</a>
+					</li>
+					<li tabIndex="0">
+						<a>
+							Parent
+							<svg
+								className="fill-current"
+								xmlns="http://www.w3.org/2000/svg"
+								width="20"
+								height="20"
+								viewBox="0 0 24 24"
+							>
+								<path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+							</svg>
+						</a>
+						<ul className="p-2 bg-base-100">
+							<li>
+								<a>Submenu 1</a>
+							</li>
+							<li>
+								<a>Submenu 2</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a>Item 3</a>
+					</li>
+				</ul>
 			</div>
 		</div>
 	);
